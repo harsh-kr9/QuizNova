da44b818bf8e1ad7ffcac131c9013db5eb759187
Harsh Kumar <harsh2004.jayant@gmail.com>
2026-02-13 16:40:01 +0530

    create user model with password hashing

---
+++ Backend/model/User.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const HistoryModel = require('./History'); // Import the History model
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please provide name'],
+        minlength: 5,
+        maxlength: 50,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: [true, 'Please provide email'],
+        validate: {
+            validator: validator.isEmail,
+            message: 'Please provide valid email'
+        }
+    },
+    password: {
+        type: String,
+        required: [true, 'Please provide password'],
+        minlength: 6,
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'user'],
+        default: 'user',
+    },
+    history: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'History', // Reference to the History model
+        required:true,
+    }]
+     // Embedding the History model
+});
+
+// UserSchema.path('history').default([]);
+
+// Hash the Password
+// UserSchema.pre('save', async function(next) {
+//     try {
+//         const salt = await bcrypt.genSalt(10);
+//         this.password = await bcrypt.hash(this.password, salt);
+//         next();
+//     } catch (error) {
+//         next(error);
+//     }
+// });
+
+UserSchema.pre('save', async function(next) {
+    if(!this.isModified('password')) return;    
+    const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+});
+
+
+UserSchema.methods.comparePassword = async function (userPassword) {
+    const isMatch = await bcrypt.compare(userPassword, this.password);
+    return isMatch;
+}
+
+const User = mongoose.model('User', UserSchema);
+
+User.createIndexes();
+
+module.exports = User;
