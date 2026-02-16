94f47ec17419560b244de66e97a261fc073f5d5f
Harsh Kumar <harsh2004.jayant@gmail.com>
2026-02-16 16:19:09 +0530

    create ai search controller with history saving

---
+++ Backend/controllers/aiController.js
@@ -0,0 +1,26 @@
+const { StatusCodes } = require('http-status-codes');
+const { createHistory } = require('./historyController'); // Correct import path
+const {search} = require('./Gemini');
+
+const searchTopic = async (req, res) => {
+    try {
+        const userId = req.user.userId;
+        const prompt = req.query.Topic;
+        const text = await search(prompt);
+        const history = await createHistory(prompt, text, userId); 
+        const histId = history._id;
+        res.status(StatusCodes.OK).json({ text, userId,histId });
+    } catch (error) {
+        console.error('SEARCH ERROR:', error);
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ 
+            error: 'Internal Server Error', 
+            details: error.message,
+            stack: error.stack
+        });
+    }
+}
+
+module.exports = {
+    searchTopic,
+    search
+};
