c38b6ea6acea2b4c813ed51fbdc888e7345f556b
Harsh Kumar <harsh2004.jayant@gmail.com>
2026-02-16 14:50:51 +0530

    wire up auth and user routes in express app

--- Backend/app.js
+++ Backend/app.js
@@ -1,17 +1,43 @@
 const express = require('express');
 require('dotenv').config();
+require('express-async-errors');
+const cookieParser = require('cookie-parser');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
+//Connect Database
+const connectDB = require('./db/connect');
+
+//routers
+const authRouter = require('./routes/authRoute');
+const userRouter = require('./routes/userRoute');
+
+//middlewares
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
 app.use(express.json());
+app.use(cookieParser(process.env.JWT_SECRET));
 
 app.get('/', (req, res) => {
   res.send('Server is running');
 });
 
+app.get('/api/v1', (req,res,next)=>{
+    res.send('testing')
+    console.log(req.signedCookies);
+})
+
+app.use('/api/v1/auth',authRouter);
+app.use('/api/v1/users',userRouter);
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
 const start = async function() {
+    await connectDB(process.env.MONGO_URL);
     try {
-        app.listen(port, console.log(`Server is listening on ${port}`));
+        app.listen(port,console.log(`Server is listening on ${port}`));
     } catch (error) {
         console.log(error);
     }
