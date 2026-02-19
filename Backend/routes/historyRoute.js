2b2c5c8fece91bdf57cdaba02d2b6a62e1d25e77
Harsh Kumar <harsh2004.jayant@gmail.com>
2026-02-19 13:48:29 +0530

    update history routes with all endpoints

--- Backend/routes/historyRoute.js
+++ Backend/routes/historyRoute.js
@@ -2,10 +2,15 @@ const express = require('express');
 const router = express.Router();
 const {authenticateUser} = require('../middleware/authentication')
 const {
-        createHistory,getAllHistory,getSingleHistory
+        createHistory,getAllHistory,getSingleHistory,savedContent,deleteHistory,regenerateHistory
 } = require('../controllers/historyController');
 
+// router.post('/createHistory',authenticateUser,createHistory);
 router.get('/getAllHistory',authenticateUser,getAllHistory);
+router.get('/saved/:id',authenticateUser,savedContent);
 router.get('/:id',authenticateUser,getSingleHistory);
+router.delete('/deleteHistory/:id',authenticateUser,deleteHistory);
+router.post('/regenerate/:id',authenticateUser,regenerateHistory);
+
 
 module.exports = router;
