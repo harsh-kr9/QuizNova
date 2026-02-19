bcdb8f4edb8a26cf6c8129487df200757cd12d52
Harsh Kumar <harsh2004.jayant@gmail.com>
2026-02-19 16:26:05 +0530

    add social login route

--- Backend/routes/authRoute.js
+++ Backend/routes/authRoute.js
@@ -4,11 +4,13 @@ const router = express.Router();
 const {
         register,
         login,
+        socialLogin,
         logout,
 } = require('../controllers/authController');
 
 router.post('/register',register);
 router.post('/login',login);
+router.post('/social-login', socialLogin);
 router.get('/logout',logout);
 
 module.exports = router;
