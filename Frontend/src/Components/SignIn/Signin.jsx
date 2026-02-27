176f05810c96b13ae94823ba9c6e201bb2d91e96
Harsh Kumar <harsh2004.jayant@gmail.com>
2026-02-27 10:06:38 +0530

    build signin form component

---
+++ Frontend/src/Components/SignIn/Signin.jsx
@@ -0,0 +1,109 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { login, googleLogin } from '../../../actions/authActions';
+import { useDispatch, useSelector } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { setLoading } from '../../../features/userSlice';
+import { auth, googleProvider } from '../../firebase';
+import { signInWithPopup } from 'firebase/auth';
+
+const Signin = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isClicked, setIsClicked] = useState(false);
+  const [error, setError] = useState(null); // Error state
+  const isLoading = useSelector((state) => state.user && state.isLoading);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsClicked(true);
+    dispatch(setLoading(true));
+
+    try {
+      await dispatch(login({ email, password }, navigate));
+      // Reset the error state upon successful login
+      setError(null);
+    } catch (error) {
+      console.error(error);
+      // Set the error state if there's an error during login
+      setError("An error occurred. Please try again.");
+    } finally {
+      dispatch(setLoading(false));
+      setIsClicked(false); // Reset isClicked after login attempt
+    }
+  };
+
+  const handleGoogleSignIn = async () => {
+    try {
+      const result = await signInWithPopup(auth, googleProvider);
+      const user = result.user;
+      const userData = {
+        name: user.displayName,
+        email: user.email,
+        photo: user.photoURL,
+      };
+      await dispatch(googleLogin(userData, navigate));
+    } catch (error) {
+      console.error(error);
+      setError("Google Sign-In failed.");
+    }
+  };
+
+  return (
+    <section className="bg-gray-900 min-h-screen flex ">
+      <div className="flex flex-col items-center justify-center  w-4/5 mx-auto md:h-screen lg:py-0">
+        <Link to="/" className="flex items-center mb-6 text-2xl font-semibold text-gray-900">
+          <img className="md:w-64 md:h-32 w-48 h-20" src="/images/logo.png" alt="logo" />
+        </Link>
+        <div className="w-full  rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight  md:text-2xl text-white">
+              Sign in to your account
+            </h1>
+            <form onSubmit={handleSubmit} className="space-y-4 md:space-y-6" action="#">
+              <div>
+                <label htmlFor="email" className="block mb-2 text-sm font-medium text-white">Your email</label>
+                <input onChange={(e) => setEmail(e.target.value)} type="email" name="email" id="email" className=" border sm:text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="name@company.com" required />
+              </div>
+              <div>
+                <label htmlFor="password" className="block mb-2 text-sm font-medium text-white">Password</label>
+                <input onChange={(e) => setPassword(e.target.value)} type="password" name="password" id="password" placeholder="••••••••" className="border sm:text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" required />
+              </div>
+
+              <button type="submit" className="w-full text-white  focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-primary-600 hover:bg-primary-700 focus:ring-primary-800" disabled={isLoading || isClicked}>
+                {isClicked ? (
+                  <>
+                    <FontAwesomeIcon icon={faSpinner} spin className="text-white mr-2" />
+                    Signing in...
+                  </>
+                ) : (
+                  "Sign in"
+                )}
+              </button>
+
+              <div className="flex items-center justify-between">
+                <hr className="w-full border-gray-600" />
+                <span className="px-2 text-gray-400 text-sm">or</span>
+                <hr className="w-full border-gray-600" />
+              </div>
+
+              <button onClick={handleGoogleSignIn} type="button" className="w-full text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center flex items-center justify-center gap-2">
+                Sign in with Google
+              </button>
+              {error && <p className="text-red-500">{error}</p>}
+              <p className="text-sm font-light text-gray-400">
+                Don’t have an account yet? <Link to="/signup" className="font-medium  hover:underline text-primary-500">Sign up</Link>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Signin;
