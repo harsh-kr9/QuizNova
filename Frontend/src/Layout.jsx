d915b41a7bab8a9ffcff3b814c1d01b6b0445dfe
Harsh Kumar <harsh2004.jayant@gmail.com>
2026-03-02 17:55:49 +0530

    update layout with header and footer

--- Frontend/src/Layout.jsx
+++ Frontend/src/Layout.jsx
@@ -1,12 +1,36 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React, { useEffect } from 'react';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import { Outlet } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { axiosInstance } from '../utils'; // Assuming axiosInstance is correctly exported from '../utils'
+import { setUser } from '../features/userSlice';
 
-const Layout = () => {
+function Layout() {
+  const dispatch = useDispatch();
+  const user = useSelector(state => state.user)
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await axiosInstance.get('/api/v1/users/showMe');
+  //       const user = response.data.user;
+  //       dispatch(setUser(user));
+  //     } catch (error) {
+  //       console.log('Error fetching user data:', error);
+  //     }
+  //   };
+
+  //   fetchData(); 
+
+  // }, []); 
+
   return (
-    <main className='app'>
-      <Outlet />
-    </main>
-  )
+    <>
+      <Header />
+      <Outlet /> 
+      <Footer />
+    </>
+  );
 }
 
-export default Layout
+export default Layout;
