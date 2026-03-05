b8a797533cfb311f5fb628c1ea2b746ea8c0dec8
Harsh Kumar <harsh2004.jayant@gmail.com>
2026-03-05 15:33:25 +0530

    add all page routes to app router

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,11 @@
-import React from 'react'
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
-import Layout from './Layout'
-import Home from './Pages/Home/Home'
+import React from 'react';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      { index: true, element: <Home /> },
-    ]
-  }
-])
-
-function App() {
-  return <RouterProvider router={router} />
+const App = () => {
+  return (
+    <div>
+      ha ha
+    </div>
+  );
 }
 
-export default App
+export default App;
