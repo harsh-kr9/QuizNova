25ab330f1f76cab68cf35ed7db77f4cb375a666b
Harsh Kumar <harsh2004.jayant@gmail.com>
2026-03-16 10:19:49 +0530

    implement health check and clean up server config

--- Backend/app.js
+++ Backend/app.js
@@ -37,7 +37,6 @@ app.use((req, res, next) => {
 
 // Add your routes and other middleware here...
 
-// TODO: add health check endpoint
 app.get('/', (req, res) => {
   res.send('Server is running');
 });
