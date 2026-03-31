468a1bf74f3a05a058f04a6279e34a7bcf7c5503
Harsh Kumar <harsh2004.jayant@gmail.com>
2026-03-31 09:39:35 +0530

    add store configuration comment

--- Frontend/store.js
+++ Frontend/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer  from "./features/userSlice";
 
+// Configure Redux store with user reducer
 export const store = configureStore({
     reducer: userReducer
 })
