a19be3fe2c4f8373c14407c160de060f3b8bd3b8
Harsh Kumar <harsh2004.jayant@gmail.com>
2026-03-31 11:06:45 +0530

    finalize axios instance config

--- Frontend/utils/index.js
+++ Frontend/utils/index.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-// Axios instance with credentials for API calls
 export const axiosInstance = axios.create({
     withCredentials: true,
     baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000'
