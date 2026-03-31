f0a3c287c7a6f2c0053c4b3cd3b500b43e3cda37
Harsh Kumar <harsh2004.jayant@gmail.com>
2026-03-31 10:16:37 +0530

    clean redux store setup

--- Frontend/store.js
+++ Frontend/store.js
@@ -1,7 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer  from "./features/userSlice";
 
-// Configure Redux store with user reducer
 export const store = configureStore({
     reducer: userReducer
 })
